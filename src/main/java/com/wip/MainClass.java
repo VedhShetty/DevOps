847bb92b9aca26377724ee0a508eb5031235a966
Vedhanth S Shetty <Jan1user249@rpsconsulting.in>
2026-01-28 12:51:27 +0530

    Jenkins File

--- src/main/java/com/wip/MainClass.java
+++ src/main/java/com/wip/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Java For Docker!!!! . THis code is pulled from WIP GitHub Repository");
+		System.out.println("Updated Code after adding jenkins code in the repository");
 	}
 
 }
